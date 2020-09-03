c7c4730e37cd0ebb26d679136aac542a394a4fa4
Jean Carlos Alarcón <jeancalarcon98@gmail.com>
2020-09-02 21:08:28 -0500

    TASK-2: Configure Nginx for redirect correctly to Manager fronted

--- frontend/src/app/app.route.ts
+++ frontend/src/app/app.route.ts
@@ -4,10 +4,11 @@ import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
 
 
-const PAGE_ROUTES : Routes = [
+const PAGE_ROUTES: Routes = [
     { path: '', component: Page1Component },
     { path: 'pagina2', component: Page2Component },
     { path: 'pagina3', component: Page3Component },
+    { path: '**', component: Page1Component },
 ];
 
 export const PAGE_ROUTING = RouterModule.forRoot(PAGE_ROUTES);
